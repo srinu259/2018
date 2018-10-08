3a15d56393ebb6ab6469a24aa2bd3a5b1da30f60
shifin <shifin.epz@gmail.com>
2018-10-08 02:41:02 -0700

    First commit

---
+++ tomcat.sh
@@ -0,0 +1,7 @@
+!#/usr/bin/bash
+
+yum install tomcat -y
+sed -i 's/port="8080"/port="8880"/' /etc/tomcat/server.xml
+yum install tomcat-webapps tomcat-admin-webapps -y
+yum install tomcat-docs-webapp tomcat-javadoc -y
+systemctl start tomcat
