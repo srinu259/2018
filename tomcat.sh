de5b805e038543bf5b7b94b262a89c2cbb0b94c6
shifin rafeeque <shifin.epz@gmail.com>
2018-10-09 01:47:14 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
-cd /home/jenkins
-wget https://rpmfind.net/linux/fedora/linux/updates/testing/27/x86_64/Packages/t/tomcat-8.0.53-1.fc27.noarch.rpm
-rpm -ivh tomcat-8.0*
+sudo yum install tomcat -y
+sudo sed -i 's/port="8080"/port="8880"/' /etc/tomcat/server.xml
+sudo yum install tomcat-webapps tomcat-admin-webapps -y
+sudo yum install tomcat-docs-webapp tomcat-javadoc -y
