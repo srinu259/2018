8753c04b2b38bbe03fc054c4879fc151e229d31b
srinu259 <Madhav.Srinivas@gmail.com>
2018-11-15 10:06:25 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,3 +4,5 @@ sudo yum install tomcat -y
 sudo sed -i 's/port="8080"/port="8880"/' /etc/tomcat/server.xml
 sudo yum install tomcat-webapps tomcat-admin-webapps -y
 sudo yum install tomcat-docs-webapp tomcat-javadoc -y
+sudo systemctl start tomcat
+sudo systemctl status tomcat
